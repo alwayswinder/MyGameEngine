31255c19fea043b5b2234c0f7f97792e873cf3a3
侯富林 <fulin.hou@yingxiong.com>
2022-04-27 18:30:04 +0800

    23.2

--- PrimalEditor/Utilities/RenderSurface/RenderSurfaceHost.cs
+++ PrimalEditor/Utilities/RenderSurface/RenderSurfaceHost.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Windows.Input;
 using System.Windows.Interop;
 
 namespace PrimalEditor.Utilities
@@ -13,13 +14,30 @@ namespace PrimalEditor.Utilities
         private readonly int _width = 800;
         private readonly int _height = 600;
         private IntPtr _renderWindowHandle = IntPtr.Zero;
+        private DelayEventTimer _resizeTimer;
         public int SurfaceId { get; private set; } = ID.INVALID_ID;
 
+        public void Resize()
+        {
+            _resizeTimer.Trigger();
+        }
+
+        private void Resize(object sender, DelayEventTimerArgs e)
+        {
+            e.RepeatEvent = Mouse.LeftButton == MouseButtonState.Pressed;
+            if (!e.RepeatEvent)
+            {
+                Logger.Log(MessageType.Info, "Resized");
+            }
+        }
         public RenderSurfaceHost(double width, double height)
         {
             _width = (int)width;
             _height = (int)height;
+            _resizeTimer = new DelayEventTimer(TimeSpan.FromMilliseconds(250.0));
+            _resizeTimer.Triggered += Resize;
         }
+
         protected override HandleRef BuildWindowCore(HandleRef hwndParent)
         {
             SurfaceId = EngineAPI.CreateRenderSurface(hwndParent.Handle, _width, _height);
--- PrimalEditor/Utilities/RenderSurface/RenderSurfaceView.xaml.cs
+++ PrimalEditor/Utilities/RenderSurface/RenderSurfaceView.xaml.cs
@@ -6,6 +6,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -18,7 +19,17 @@ namespace PrimalEditor.Utilities
     /// </summary>
     public partial class RenderSurfaceView : UserControl, IDisposable
     {
+        private enum Win32Msg
+        {
+            WM_SIZING = 0x0214,
+            WM_ENTERSIZEMOVE = 0x0231,
+            WM_EXITSIZEMOVE = 0x0232,
+            WM_SIZE  = 0x0005,
+        }
+
         private RenderSurfaceHost _host = null;
+        private bool _canResize = true;
+        private bool _moved = false;
 
         public RenderSurfaceView()
         {
@@ -30,7 +41,58 @@ namespace PrimalEditor.Utilities
         {
             Loaded -= OnRenderSurfaceViewLoaded;
             _host = new RenderSurfaceHost(ActualWidth, ActualHeight);
+            _host.MessageHook += new HwndSourceHook(HostMsgFilter);
             Content = _host;
+
+            var window = Application.Current.MainWindow;
+            var helper = new WindowInteropHelper(window);
+            if(helper.Handle !=null)
+            {
+                HwndSource.FromHwnd(helper.Handle)?.AddHook(HwndMessageHook);
+            }
+        }
+
+        private IntPtr HwndMessageHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch ((Win32Msg)msg)
+            {
+                case Win32Msg.WM_SIZING:
+                    _canResize = false;
+                    _moved = false;
+                    break;
+                case Win32Msg.WM_ENTERSIZEMOVE:
+                    _moved = true;
+                    break;
+                case Win32Msg.WM_EXITSIZEMOVE:
+                    _canResize = true;
+                    if(_moved)
+                    {
+                        _host.Resize();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+
+        private IntPtr HostMsgFilter(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch ((Win32Msg)msg)
+            {
+                case Win32Msg.WM_SIZING: throw new Exception();
+                case Win32Msg.WM_ENTERSIZEMOVE: throw new Exception();
+                case Win32Msg.WM_EXITSIZEMOVE: throw new Exception();
+                case Win32Msg.WM_SIZE:
+                    if (_canResize)
+                    {
+                        _host.Resize();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
         }
 
         #region IDisposable support
