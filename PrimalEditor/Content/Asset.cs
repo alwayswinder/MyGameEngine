5cb79d4b932161d0dcd1e43960b3c4aa2b90db22
alwayswinder <1181612350@qq.com>
2022-05-18 15:40:54 +0800

    25.2

---
+++ PrimalEditor/Content/Asset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace PrimalEditor.Content
+{
+    enum AssetType
+    {
+        Unknown,
+        Animation,
+        Audio,
+        Material,
+        Mesh,
+        Skeleton,
+        Texture,
+    }
+    abstract class Asset : ViewModeBase
+    {
+        public AssetType Type { get; private set; }
+        public Asset(AssetType type)
+        {
+            Debug.Assert(type != AssetType.Unknown);
+            Type = type;
+        }
+    }
+}
---
+++ PrimalEditor/DllWrapper/ContentToolsAPI.cs
@@ -0,0 +1,82 @@
+﻿using PrimalEditor.ContentToolsAPIStructs;
+using PrimalEditor.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+namespace PrimalEditor.ContentToolsAPIStructs
+{
+    [StructLayout(LayoutKind.Sequential)]
+    class GeometryImportSettings
+    {
+       public float smoothingAngle = 178f;
+       public byte calculateNormals = 0;
+       public byte calculateTangents = 1;
+       public byte reverseHandedness = 0;
+       public byte importEmbeded_textures = 1;
+       public byte importAnimations = 1;
+    }
+
+
+    [StructLayout(LayoutKind.Sequential)]
+    class SceneData : IDisposable
+    {
+        public IntPtr Data;
+        public int DataSize;
+        public GeometryImportSettings ImportSettings = new GeometryImportSettings();
+
+        public void Dispose()
+        {
+            Marshal.FreeCoTaskMem(Data);
+            GC.SuppressFinalize(this);
+        }
+        ~SceneData()
+        {
+            Dispose();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    class PrimitiveInitInfo
+    {
+        public Content.PrimitiveMeshType Type;
+        public int SegmentX = 1;
+        public int SegmentY = 1;
+        public int SegmentZ = 1;
+        public Vector3 Size = new Vector3(1f);
+        public int LOD = 0;
+    }
+}
+namespace PrimalEditor.DllWrappers
+{
+    static class ContentToolsAPI
+    {
+        private const string _toolsDLL = "ContentTools.dll";
+
+        [DllImport(_toolsDLL)]
+        private static extern void CreatePrimitiveMesh([In, Out] SceneData data, PrimitiveInitInfo info);
+
+        public static void CreatePrimitiveMesh(Content.Geometry geometry, PrimitiveInitInfo info)
+        {
+            Debug.Assert(geometry != null);
+            using var sceneData = new SceneData();
+            try
+            {
+                CreatePrimitiveMesh(sceneData, info);
+                Debug.Assert(sceneData.Data != IntPtr.Zero && sceneData.DataSize > 0);
+                var data = new byte[sceneData.DataSize];
+                Marshal.Copy(sceneData.Data, data, 0, sceneData.DataSize);
+                geometry.FromRawData(data);
+            }
+            catch(Exception ex)
+            {
+                Logger.Log(MessageType.Error, $"failed to create {info.Type} primitive mesh.");
+                Debug.WriteLine(ex.Message);
+            }
+        }
+    }
+}
