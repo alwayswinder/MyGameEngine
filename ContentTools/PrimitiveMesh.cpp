a92ea488571f6be0801b38084ef4dc4e9da57bd8
alwayswinder <1181612350@qq.com>
2022-05-25 16:19:58 +0800

    27.1

--- ContentTools/PrimitiveMesh.cpp
+++ ContentTools/PrimitiveMesh.cpp
@@ -6,6 +6,7 @@ namespace primal::tools
 	namespace
 	{
 		using namespace math;
+		using namespace DirectX;
 		using primitive_mesh_creator = void(*)(scene&, const primitive_init_info& info);
 
 		void create_plane(scene& scene, const primitive_init_info& info);
@@ -110,6 +111,111 @@ namespace primal::tools
 			}
 			return m;
 		}
+		mesh create_uv_sphere(const primitive_init_info& info)
+		{
+			const u32 phi_count{ clamp(info.segments[axis::x], 3u, 64u) };
+			const u32 theta_count{ clamp(info.segments[axis::y], 2u, 64u) };
+			const f32 theta_steps{ pi / theta_count };
+			const f32 phi_steps{ two_pi / phi_count };
+			const u32 num_indices{ 2 * 3 * phi_count + 2 * 3 * phi_count * (theta_count - 2) };
+			const u32 num_vertices{ 2 + phi_count * (theta_count - 1) };
+
+			mesh m{};
+			m.name = "uv_sphere";
+			m.positions.resize(num_vertices);
+
+			//add the top vertex
+			u32 c{ 0 };
+			m.positions[c++] = { 0.f, info.size.y, 0.f };
+
+
+			for (u32 j{ 1 }; j<=(theta_count -1); ++j)
+			{
+				const f32 theta{ j * theta_steps };
+				for (u32 i{0}; i< phi_count; ++i)
+				{
+					const f32 phi{ i * phi_steps };
+					m.positions[c++] =
+					{
+						info.size.x * XMScalarSin(theta) * XMScalarCos(phi),
+						info.size.y * XMScalarCos(theta),
+						-info.size.z * XMScalarSin(theta) * XMScalarSin(phi)
+					};
+				}
+			}
+			//add bottom vertex
+			m.positions[c++] = { 0.f, -info.size.y, 0.f };
+			assert(c == num_vertices);
+
+			c = 0;
+			m.raw_indices.resize(num_indices);
+
+			for (u32 i{ 0 }; i< phi_count -1; ++i)
+			{
+				m.raw_indices[c++] = 0;
+				m.raw_indices[c++] = i + 1;
+				m.raw_indices[c++] = i + 2;
+			}
+
+			m.raw_indices[c++] = 0;
+			m.raw_indices[c++] = phi_count;
+			m.raw_indices[c++] = 1;
+
+			for (u32 j{ 0 };j<(theta_count - 2); ++j)
+			{
+				for (u32 i{0}; i< (phi_count - 1); ++i)
+				{
+					const u32 index[4]
+					{
+						1 + i + j * phi_count,
+						1 + i + (j + 1) * phi_count,
+						1 + (i + 1) + (j + 1) * phi_count,
+						1 + (i + 1) + j * phi_count
+					};
+					m.raw_indices[c++] = index[0];
+					m.raw_indices[c++] = index[1];
+					m.raw_indices[c++] = index[2];
+
+					m.raw_indices[c++] = index[0];
+					m.raw_indices[c++] = index[2];
+					m.raw_indices[c++] = index[3];
+				}
+
+				const u32 index[4]
+				{
+					phi_count + j * phi_count,
+					phi_count + (j + 1) * phi_count,
+					1 + (j + 1) * phi_count,
+					1 + j * phi_count
+				};
+				m.raw_indices[c++] = index[0];
+				m.raw_indices[c++] = index[1];
+				m.raw_indices[c++] = index[2];
+
+				m.raw_indices[c++] = index[0];
+				m.raw_indices[c++] = index[2];
+				m.raw_indices[c++] = index[3];
+			}
+
+			const u32 south_pole_index{ (u32)m.positions.size() - 1 };
+			for (u32 i{0}; i<(phi_count -1); ++i)
+			{
+				m.raw_indices[c++] = south_pole_index;
+				m.raw_indices[c++] = south_pole_index - phi_count + i + 1;
+				m.raw_indices[c++] = south_pole_index - phi_count + i;
+			}
+
+			m.raw_indices[c++] = south_pole_index;
+			m.raw_indices[c++] = south_pole_index - phi_count;
+			m.raw_indices[c++] = south_pole_index - 1;
+
+			m.uv_sets.resize(1);
+			m.uv_sets[0].resize(m.raw_indices.size());
+
+			return m;
+		}
+
+		/**********************/
 		void create_plane(scene& scene, const primitive_init_info& info)
 		{
 			lod_group lod{};
@@ -125,7 +231,10 @@ namespace primal::tools
 			
 		void create_uv_sphere(scene& scene, const primitive_init_info& info)
 		{
-
+			lod_group lod{};
+			lod.name = "uv_sphere";
+			lod.meshes.emplace_back(create_uv_sphere(info));
+			scene.lod_groups.emplace_back(lod);
 		}
 
 		void create_ico_sphere(scene& scene, const primitive_init_info& info)
