5d17817fe00cc8ccef67a9e2532ba67f017887bf
侯富林 <fulin.hou@yingxiong.com>
2022-06-22 10:47:34 +0800

    platform

---
+++ Engine/Engine/Platform/IncludeWindowCpp.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+#define INCLUDE_WINDOW_CPP 1
+#include "Window.cpp"
+#undef INCLUDE_WINDOW_CPP
--- Engine/Engine/Platform/PlatformWin32.cpp
+++ Engine/Engine/Platform/PlatformWin32.cpp
@@ -1,9 +1,11 @@
+
+#ifdef _WIN64
 #include "Platform.h"
 #include "PlatformTypes.h"
 
 namespace primal::platform
 {
-#ifdef _WIN64
+
 	namespace
 	{
 		struct window_info
@@ -221,53 +223,7 @@ namespace primal::platform
 		DestroyWindow(info.hwnd);
 		windows.remove(id);
 	}
-#else
-#error "must implement at least one platform"
 #endif//_WIN64
 
-	void  window::set_fullscreen(bool is_fullscreen)const
-	{
-		assert(is_valid());
-		set_window_fullscreen(_id, is_fullscreen);
-	}
-	bool  window::is_fullscreen()const
-	{
-		assert(is_valid());
-		return is_window_fullscreen(_id);
-	}
-	void* window::handle()const
-	{
-		assert(is_valid());
-		return get_window_handle(_id);
-	}
-	void  window::set_caption(const wchar_t* caption)const
-	{
-		assert(is_valid());
-		return set_window_caption(_id, caption);
-	}
-	const math::u32v4 window::size()const
-	{
-		assert(is_valid());
-		return get_window_size(_id);
-	}
-	void  window::resize(u32 width, u32 height)const
-	{
-		assert(is_valid());
-		resize_window(_id, width, height);
-	}
-	u32 window::width()const
-	{
-		math::u32v4 s{ size() };
-		return s.z - s.x;
-	}
-	u32 window::height()const
-	{
-		math::u32v4 s{ size() };
-		return s.w - s.y;
-	}
-	bool  window::is_closed()const
-	{
-		assert(is_valid());
-		return is_window_closed(_id);
-	}
+#include "IncludeWindowCpp.h"
 }
